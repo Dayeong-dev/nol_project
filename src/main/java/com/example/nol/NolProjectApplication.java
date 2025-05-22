c57390a5cea117cfdace05f7c0fbf48a14a4fdad
Yun Dayeong <ydiosa98@gmail.com>
2025-05-22 14:48:59 +0900

    first commit

---
+++ src/main/java/com/example/nol/NolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.nol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolProjectApplication.class, args);
+	}
+
+}
